9d4d2c9496cf1f73f5d909c212e5f2df4c9ffe9a
yegyeom yu <meopulior@gmail.com>
2018-07-12 22:57:05 +0900

    Chapter 01 소개, 파일 생성, 디버깅 끝

---
+++ Chapter01/Chapter01/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//#include <string>
+
+using namespace std;
+
+int main() {
+	int x, y, z;
+	cout << "첫 번째 수 : ";
+	cin >> x;
+	cout << "두 번째 수 : ";
+	cin >> y;
+	z = x + y;
+	cout << "두 수의 합 : " << z << endl;
+	cout << "두 수의 곱 : " << x * y << endl;
+	return 0;
+}
